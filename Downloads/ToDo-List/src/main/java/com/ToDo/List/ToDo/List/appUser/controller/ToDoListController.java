78f2644a516b7f390470a22d7f3a37839cd3430b
Favourmbata <favourmbata520@gmail.com>
2023-12-28 16:32:07 +0100

    firstcommit

--- Downloads/ToDo-List/src/main/java/com/ToDo/List/ToDo/List/appUser/controller/ToDoListController.java
+++ Downloads/ToDo-List/src/main/java/com/ToDo/List/ToDo/List/appUser/controller/ToDoListController.java
@@ -1,9 +1,14 @@
 package com.ToDo.List.ToDo.List.appUser.controller;
 
+import com.ToDo.List.ToDo.List.appUser.service.interfaces.AppUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
-public class ToDoListController {
+@RestController
+@RequiredArgsConstructor
 
+public class ToDoListController {
+   private final AppUserService appUserService;
 
 
 
